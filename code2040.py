4785bd80bd4a99d28ee30ee311dbf40220ee022e
Jarod Robinson <jarodrobinson707@gmail.com>
2016-10-20 00:58:28 -0700

    Update code2040.py

--- code2040.py
+++ code2040.py
@@ -9,10 +9,9 @@ def main():
     github = 'https://github.com/jdrobinson707/hello-jarod'
     url = 'http://challenge.code2040.org/api/register'
     token = '66d5f9f688c97246594893b381490a59'
-    payload = { 'token': '66d5f9f688c97246594893b381490a59',
-             'github': 'https://github.com/jdrobinson707/hello-jarod' }
-    r = requests.post(url, data=json.dumps(payload))
-    
+    payload = { token: '66d5f9f688c97246594893b381490a59',
+             github: 'https://github.com/jdrobinson707/hello-jarod }
+    requests.post(url, data=json.dumps(payload))
 
 main()
 
