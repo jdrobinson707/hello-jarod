d58e501542b49307d3e47af39207f6303e2d7187
Jarod Robinson <jarodrobinson707@gmail.com>
2016-10-20 00:51:52 -0700

    Update code2040.py

--- code2040.py
+++ code2040.py
@@ -1,11 +1,7 @@
 #code2040 fellows application
 
 import json, requests
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
-
+     
 def main():
     #--------------------
     #Step 1: Registration
@@ -13,12 +9,11 @@ def main():
     github = 'https://github.com/jdrobinson707/hello-jarod'
     url = 'http://challenge.code2040.org/api/register'
     token = '66d5f9f688c97246594893b381490a59'
-    data = { 'github': github, 'token': token }
-    headers = {'Content-type': 'application/json'}
+    payload = { 'token': '66d5f9f688c97246594893b381490a59',
+             'github': 'https://github.com/jdrobinson707/hello-jarod' }
+    r = requests.post(url, data=json.dumps(payload))
+    
 
-    try:
-        response = requests.post(url, data=json.dumps(data), headers=headers)
-    except requests.ConnectionError:
-        sys.exit()
 main()
+
 
